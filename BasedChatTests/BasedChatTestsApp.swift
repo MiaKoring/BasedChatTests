afe63fae03d0415db07d654c85fe54c4a96cf5cd
MiaKoring <143010553+MiaKoring@users.noreply.github.com>
2024-05-01 20:45:35 +0200

    Initial Commit

---
+++ BasedChatTests/BasedChatTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasedChatTestsApp.swift
+//  BasedChatTests
+//
+//  Created by Mia Koring on 01.05.24.
+//
+
+import SwiftUI
+
+@main
+struct BasedChatTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasedChatTests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasedChatTests
+//
+//  Created by Mia Koring on 01.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
